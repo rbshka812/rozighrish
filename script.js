db35e34f28cb471e63e4cf2664fe21ba75124fe6
rbshka812 <pro245mmmgbe@gmail.com>
2025-07-25 19:31:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('prizeForm').addEventListener('submit', function (e) {
     return;
   }
 
-  fetch('https://proxylast-1.onrender.com/', {
+  fetch('https://proxylast-2.onrender.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name, phone })
